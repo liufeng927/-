10a8eea45dad6c66a1e2c25ddd0450676eec6198
liufeng <1404476833@qq.com>
2020-04-27 16:35:30 +0800

    first commit

---
+++ demo008/src/Main.java
@@ -0,0 +1,10 @@
+/**
+ * @description:
+ * @Date: 2020-04-27 16:29
+ * @author: liufeng
+ **/
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hello!");
+    }
+}
